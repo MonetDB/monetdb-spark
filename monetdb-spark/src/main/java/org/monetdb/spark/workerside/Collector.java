c30968762cc1cc8e94941cb9a37d4b1c933ffadf
Joeri van Ruth <joeri.van.ruth@monetdbsolutions.com>
2026-01-23 15:53:31 +0100

    Support CompressionSettings in Collector

--- monetdb-spark/src/main/java/org/monetdb/spark/workerside/Collector.java
+++ monetdb-spark/src/main/java/org/monetdb/spark/workerside/Collector.java
@@ -12,6 +12,7 @@ package org.monetdb.spark.workerside;
 
 import org.apache.spark.unsafe.types.UTF8String;
 import org.monetdb.jdbc.MonetConnection;
+import org.monetdb.spark.common.CompressionSettings;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
@@ -20,7 +21,8 @@ import java.math.BigInteger;
 import java.util.ArrayList;
 
 public final class Collector implements MonetConnection.UploadHandler {
-	final ArrayList<ByteArrayOutputStream> buffers;
+	private final CompressionSettings compressionSettings;
+	private final ArrayList<ByteArrayOutputStream> buffers;
 	private int rowCount = 0;
 
 	public boolean scratchNull;
@@ -34,6 +36,11 @@ public final class Collector implements MonetConnection.UploadHandler {
 	private Runnable onEndUpload;
 
 	public Collector() {
+		this(null);
+	}
+
+	public Collector(CompressionSettings compressionSettings) {
+		this.compressionSettings = compressionSettings;
 		buffers = new ArrayList<>();
 	}
 
@@ -81,7 +88,11 @@ public final class Collector implements MonetConnection.UploadHandler {
 		onStartUpload.run();
 		try {
 			OutputStream stream = handle.getStream();
+			if (compressionSettings != null) {
+				stream = compressionSettings.wrap(stream);
+			}
 			writeTo(idx, stream);
+			stream.close();
 		} finally {
 			onEndUpload.run();
 		}
