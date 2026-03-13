1ec2d97d228ecdf48601c9c30d23dcf5631e3e30
Joeri van Ruth <joeri.van.ruth@monetdbsolutions.com>
2026-03-13 13:49:22 +0100

    Enable all MonetSaveModeTests

--- monetdb-spark/src/test/java/org/monetdb/spark/playground/MonetSaveModeTests.java
+++ monetdb-spark/src/test/java/org/monetdb/spark/playground/MonetSaveModeTests.java
@@ -1,69 +1,9 @@
 package org.monetdb.spark.playground;
 
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
-
-import java.sql.SQLException;
-
 public class MonetSaveModeTests extends AbstractSaveModeTests {
 
 	@Override
 	protected String getSource() {
 		return "org.monetdb.spark";
-	}
-	@Test
-	@Disabled("mode doesn't work yet")
-	@Override
-	public void testOverwriteWhenPresent() throws SQLException {
-		super.testOverwriteWhenPresent();
-	}
-
-	@Test
-	@Disabled("mode doesn't work yet")
-	@Override
-	public void testOverwriteWhenNotPresent() throws SQLException {
-		super.testOverwriteWhenNotPresent();
-	}
-
-	@Test
-	@Disabled("mode doesn't work yet")
-	@Override
-	public void testForeignKeyWithoutTruncate() throws SQLException {
-		super.testForeignKeyWithoutTruncate();
-	}
-
-	@Test
-	@Disabled("mode doesn't work yet")
-	@Override
-	public void testForeignKeyWithTruncateCascade() throws SQLException {
-		super.testForeignKeyWithTruncateCascade();
-	}
-
-	@Test
-	@Disabled("mode doesn't work yet")
-	@Override
-	public void testForeignKeyWithTruncateNoData() throws SQLException {
-		super.testForeignKeyWithTruncateNoData();
-	}
-
-	@Test
-	@Disabled("mode doesn't work yet")
-	@Override
-	public void testForeignKeyWithTruncateWithData() throws SQLException {
-		super.testForeignKeyWithTruncateWithData();
-	}
-
-	@Test
-	@Disabled("mode doesn't work yet")
-	@Override
-	public void testOverwriteTruncateWhenNotPresent() throws SQLException {
-		super.testOverwriteTruncateWhenNotPresent();
-	}
-
-	@Test
-	@Disabled("mode doesn't work yet")
-	@Override
-	public void testOverwriteTruncateWhenPresent() throws SQLException {
-		super.testOverwriteTruncateWhenPresent();
 	}
 }
