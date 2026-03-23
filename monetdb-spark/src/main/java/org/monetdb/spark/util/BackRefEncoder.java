dad4d10272e52352818630bf594eecfe9562387f
Joeri van Ruth <joeri.van.ruth@monetdbsolutions.com>
2026-03-23 10:26:51 +0100

    Implement backref encoder for multi-byte strings

--- monetdb-spark/src/main/java/org/monetdb/spark/util/BackRefEncoder.java
+++ monetdb-spark/src/main/java/org/monetdb/spark/util/BackRefEncoder.java
@@ -9,6 +9,23 @@ public class BackRefEncoder {
     private final ByteBuffer encoded = ByteBuffer.allocate(16);
     private long itemCounter = 1; // valid values are > 0
     private final long[] lastShort = new long[129]; // pos 0 is used for empty, 128 for null
+    private Cache currentCache;
+    private Cache previousCache;
+
+    /**
+     * Construct a BackRefEncoder.
+     * @param withCache if true, the backref encoder tracks multi-character strings.
+     */
+    public BackRefEncoder(boolean multiChar) {
+        if (multiChar) {
+            currentCache = new Cache();
+            previousCache = new Cache();
+        }
+    }
+
+    public BackRefEncoder() {
+        this(true);
+    }
 
     /**
      * Write the item to the stream, hopefully backref encoded.
@@ -40,17 +57,17 @@ public class BackRefEncoder {
         long itemNr = itemCounter++;
 
         if (item == null) {
-            return recordShort(128, itemNr);
+            return examineShort(128, itemNr);
         } else if (item.limit() == 0) {
-            return recordShort(0, itemNr);
+            return examineShort(0, itemNr);
         } else if (item.position() == 0 && item.limit() == 1 && item.get(0) >= 0) {
-            return recordShort(item.get(0), itemNr);
+            return examineShort(item.get(0), itemNr);
         } else {
-            return false;
+            return examineLong(item, itemNr);
         }
     }
 
-    private boolean recordShort(int b, long itemNr) {
+    private boolean examineShort(int b, long itemNr) {
         if (lastShort[b] == 0) {
             lastShort[b] = itemNr;
             return false;
@@ -58,30 +75,137 @@ public class BackRefEncoder {
         // Remember we saw it and compute the delta
         long delta = itemNr - lastShort[b];
         lastShort[b] = itemNr;
-
-        encode(delta);
-        return true;
+        return encode(delta);
     }
 
-    private void encode(long delta) {
+
+    private boolean examineLong(ByteBuffer item, long itemNr) {
+        if (currentCache == null)
+            return false;
+        if (item.remaining() > currentCache.MAX_ITEM_SIZE)
+            return false;
+        int hash = item.hashCode();
+
+        // First look it up in the current cache. If we have it we're done
+        long earlier;
+        earlier = currentCache.lookup(item, hash, itemNr);
+        if (earlier > 0) {
+            return encode(itemNr - earlier);
+        } else {
+            // Maybe the previous cache has it. In that case we can use that number.
+            // Next time we'll find it in our current cache.
+            earlier = previousCache.lookup(item, hash, itemNr);
+        }
+
+        // Before we insert we may have to make room
+        if (!currentCache.fits(item)) {
+            Cache tmp = previousCache;
+            previousCache = currentCache;
+            currentCache = tmp;
+            currentCache.reset();
+        }
+        currentCache.append(item, hash, itemNr);
+
+        // 'earlier' has been -1 but may have been overwritten by the second cache
+        if (earlier > 0)
+            return encode(itemNr - earlier);
+        else
+            return false;
+    }
+
+    private boolean encode(long delta) {
         encoded.clear();
         if (delta < 64) {
             encoded.put((byte)(0x80 + delta));
-            encoded.flip();
-            return;
-        }
-        encoded.put((byte)0x80);
-        while (delta > 0) {
-            long chunk = delta % 128;
-            if (chunk < delta)
-                delta += 0x80;
-            encoded.put((byte) chunk);
-            delta /= 128;
+        } else {
+            encoded.put((byte) 0x80);
+            while (delta > 0) {
+                long chunk = delta % 128;
+                if (chunk < delta) {
+                    // not last
+                    chunk += 0x80;
+                }
+                encoded.put((byte) chunk);
+                delta /= 128;
+            }
         }
         encoded.flip();
+        return true;
     }
 
     public ByteBuffer encoded() {
         return encoded;
+    }
+
+    private class Cache {
+        private final int SIZE = 2 << 20;
+        private final int MAX_ITEM_SIZE = SIZE / 3;
+        private final int NSLOTS = 1 << 16;
+        private final int[] offsets;
+        private final long[] itemNumbers;
+        private final ByteBuffer text;
+
+        private Cache() {
+            offsets = new int[NSLOTS];
+            itemNumbers = new long[NSLOTS];
+            text = ByteBuffer.allocate(SIZE);
+            text.position(1); // 0 reserved for 'not present'
+        }
+
+        void reset() {
+            text.position(1); // 0 reserved for 'not present'
+            for (int i = 0; i < NSLOTS; i++)
+                offsets[i] = 0;  // not present anymore
+        }
+
+        /**
+         * Find the item in the cache and return its item number.
+         * Also update the item number to the newer value 'itemNr'
+         */
+        long lookup(ByteBuffer s, int hash, long itemNr) {
+            int index = hashToIndex(hash);
+            int offset = offsets[index];
+            if (offset == 0) {
+                // not present
+                return -1;
+            }
+            int len = s.remaining();
+            if (text.position() - offset < len + 1) {
+                // s is too long, this positino can't be a match
+                return -1;
+            }
+            if (text.get(offset + len) != 0) {
+                // we expect a trailing NUL byte there
+                return -1;
+            }
+            if (s.compareTo(text.slice(offset, len)) != 0) {
+                // text doesn't match
+                return -1;
+            }
+
+            // We have found a previous occurrence.
+            // Return it but from now on remember the more current one.
+            long previous = itemNumbers[index];
+            itemNumbers[index] = itemNr;
+            return previous;
+        }
+
+        boolean fits(ByteBuffer s) {
+            return text.remaining() >= s.remaining() + 1;
+        }
+
+        void append(ByteBuffer s, int hash, long itemNr) {
+            int index = hashToIndex(hash);
+            offsets[index] = text.position();
+            itemNumbers[index] = itemNr;
+            text.put(s.duplicate());
+            text.put((byte)0);
+        }
+
+        private int hashToIndex(int hash) {
+            // Hash can be negative!
+            long neverNegative = (1L<<32) + hash;
+            return (int)(neverNegative % NSLOTS);
+        }
     }
 }
--- monetdb-spark/src/test/java/org/monetdb/spark/util/BackRefEncoderTests.java
+++ monetdb-spark/src/test/java/org/monetdb/spark/util/BackRefEncoderTests.java
@@ -5,9 +5,11 @@ import org.junit.jupiter.api.Test;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.nio.ByteBuffer;
+import java.util.ArrayList;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.monetdb.spark.util.Assertions.dollarEscape;
 
 class BackRefEncoderTests {
@@ -44,4 +46,77 @@ class BackRefEncoderTests {
     public void testNoRepeatMulti() throws IOException {
         assertEquals("aap$00" + "noot$00" + "$80$00" + "$00" +"mies$00", qencode("aap", "noot", null, "", "mies"));
     }
+
+    @Test
+    public void testFarBack() throws IOException {
+        int n = 3000;
+        String[] inputs = new String[n];
+        String[] expected = new String[n];
+
+        // Set up basic non-repeating data
+        for (int i = 0; i < n; i++) {
+            String s = "s" + i;
+            inputs[i] = s;
+            expected[i] = s + "$00";
+        }
+
+        // Introduce some repetitions:
+
+        // Short
+        inputs[100] = inputs[100 - 30];
+        expected[100] = checkDelta(new byte[] { -0x80 + 30 }, 30);
+        inputs[200] = inputs[200 - 63];
+        expected[200] = checkDelta(new byte[] { -0x80 + 63 }, 63);
+
+        // Long
+        inputs[300] = inputs[300 - 64];
+        expected[300] = checkDelta(new byte[] { -0x80, 64 }, 64);
+        inputs[1000] = inputs[1000 - 500];
+        // 500 = 3 * 128 + 116.
+        expected[1000] = checkDelta(new byte[] { -0x80, -0x80 + 116, 3 }, 500);
+
+        StringBuilder builder = new StringBuilder();
+        for (String e: expected) {
+            builder.append(e);
+        }
+        String fullExpected = builder.toString();
+        assertEquals(fullExpected, qencode(inputs));
+    }
+
+    private String checkDelta(byte[] bytes, long expected) {
+        // Short or long?
+        if (bytes.length == 1) {
+            // Short
+            byte b = bytes[0];
+            byte lo = -0x80 + 1;
+            byte hi = -0x80 + 63;
+            assertTrue(lo <= b && b <= hi, "lo=" + lo + " b=" + b + " hi=" + hi);
+            assertEquals(expected, (long)(b & 0x7F));
+        } else {
+
+            // Long.
+            // The first byte is always 0x80
+            assertEquals(-0x80, bytes[0]);
+
+            // All but the last byte have their high bit set
+            for (int i = 1; i < bytes.length - 1; i++) {
+                assertTrue(bytes[i] < 0, "bytes[" + i + "]=" + bytes[i]);
+            }
+            assertTrue(bytes[bytes.length - 1] >= 0);
+
+            // The encoded number is what we expect
+            long acc = 0;
+            int shift = 0;
+            for (int i = 1; i < bytes.length; i++) {
+                long payload = bytes[i] & 0x7F;
+                acc += payload << shift;
+                shift += 7;
+            }
+            assertEquals(expected, acc);
+        }
+
+        // For convenience, we return the dollar encoding
+        return dollarEscape(bytes);
+    }
+
 }
